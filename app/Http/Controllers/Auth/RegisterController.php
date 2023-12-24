f7e0d89142090a9c3ca8a3ebf9f866bb877e1576
Breif7 <miguelmtzo1701@gmail.com>
2023-12-24 00:00:47 -0600

    Correcion de errores y funcion adecuada registro usuario nuevo

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Database\QueryException;
 use App\Http\Controllers\Controller;
 
 
@@ -15,19 +15,38 @@ class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
+        Log::info('Register attempt', $request->all());
+
+        // Define las reglas de validación
+        $rules = [
+            'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
-        ]);
+        ];
 
-        
-        $user = User::create([
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-        ]);
+        $validator = Validator::make($request->all(), $rules);
 
-        Log::info('User successfully registered', ['user_id' => $user->id, 'email' => $user->email]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        try {
+            $validatedData = $validator->validated();
 
-        return response()->json(['message' => 'User successfully registered', 'user' => $user]);
-            }
+            Log::info('Validation', $validatedData);
+            
+            $user = new User();
+            $user->name = $validatedData['name'];
+            $user->email = $validatedData['email'];
+            $user->password = Hash::make($validatedData['password']);
+            
+            $user->save();
+
+            Log::info('User successfully registered', ['user_id' => $user->id, 'email' => $user->email]);
+
+            return response()->json(['message' => 'User successfully registered', 'user' => $user]);
+        } catch (QueryException $e) {
+            Log::error('User register failed', ['error' => $e->getMessage()]);
+            return response()->json(['message' => 'User register failed'], 500);
+        }
+    }
 }
